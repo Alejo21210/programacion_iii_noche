58938ca7ac7d9917810f95514c19f1febf762611
Alejo21210 <johan2186745@gmail.com>
2025-12-10 21:51:11 -0500

    clase-2025-12-10

---
+++ modulo_3/reacjs/my-react-app/src/App.tsx
@@ -0,0 +1,29 @@
+import ButtonCounter from "./components/ButtonCounter";
+import ColorBox from "./components/ColorBox";
+import ConditionalText from "./components/ConditionalText";
+import EmojiList from "./components/EmojiList";
+import HelloWorld from "./components/HelloWorld";
+import ShowDate from "./components/ShowDate";
+import SimpleForm from "./components/SimpleForm";
+import TextInput from "./components/TextInput";
+import ToggleMessage from "./components/ToggleMessage";
+import WelcomeUser from "./components/WelcomeUser";
+
+function App() {
+  return (
+    <>
+    <HelloWorld/>
+    <WelcomeUser/>
+    <ButtonCounter/>
+    <TextInput/>
+    <ToggleMessage/>
+    <ShowDate/>
+    <ColorBox/>
+    <ConditionalText/>
+    <EmojiList/>
+    <SimpleForm/>
+    </>
+  )
+} 
+export default App
+
---
+++ modulo_3/reacjs/my-react-app/src/components/WelcomeUser.tsx
@@ -0,0 +1,5 @@
+export default function WelcomeUser() {
+  const name = "Alejandro";
+  const apellido = "Martinez";
+  return <p>Bienvenido, {name} {apellido}</p>;
+}
