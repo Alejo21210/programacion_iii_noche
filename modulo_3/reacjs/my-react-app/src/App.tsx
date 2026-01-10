61eb3e454d25d6b81e315ac0e348256c2f9a0214
Alejo21210 <johan2186745@gmail.com>
2026-01-09 21:55:47 -0500

    clase 2026-01-09

--- modulo_3/reacjs/my-react-app/src/App.tsx
+++ modulo_3/reacjs/my-react-app/src/App.tsx
@@ -1,46 +1,25 @@
-import Clock from "./hooks/useEffect/Clock";
-import DynamicTitle from "./hooks/useEffect/DynamicTitle";
-import FetchUser from "./hooks/useEffect/FetchUser";
-import LogEffect from "./hooks/useEffect/LogEffect";
-import SafeFetch from "./hooks/useEffect/SafeFetch";
-import ScrollLogger from "./hooks/useEffect/ScrollLogger";
-import AccessWithLimit from "./hooks/useState/AccessWithLimit";
-import CheckboxSummary from "./hooks/useState/CheckboxSummary";
-import DocumentTitleChanger from "./hooks/useState/DocumentTitleChanger";
-import HoverFont from "./hooks/useState/HoverFont";
-import ParentLoginForm from "./hooks/useState/ParentLoginForm";
-import PostLikes from "./hooks/useState/PostLikes";
-import CalculadoraTrapecio from "./hooks/useState/áreadeuntrapecio";
-import ButtonOne from "./props/ButtonOne";
-import Greeting from "./props/Greeting";
-import ParentIncrementButton from "./props/ParentIncrementButton";
-import ParentInputNotifier from "./props/ParentInputNotifier";
-import UserCard from "./props/UserCard";
+import Contador from "./hooks/useCallback/Contador";
+import ExpensiveCalc from "./hooks/useMemo/ExpensiveCalc";
+import FiltroLista from "./hooks/useMemo/FiltroLista";
+import CirculoColorido from "./hooks/useRef/CirculoColorido";
+import FocoAutomatico from "./hooks/useRef/FocoAutomatico";
+import MoverCaja from "./hooks/useRef/MoverCaja";
+import ReproductorSonido from "./hooks/useRef/ReproductorSonido";
+import ScrollDemo from "./hooks/useRef/ScrollDemo";
 
 function App() {
-  const userData = { name: "Luis", age: 35 };
   return (
     <>
-      <HoverFont/>
-      <SafeFetch/>
-      <ScrollLogger/>
-      <DynamicTitle/>
-      <Clock/>
-      <FetchUser/>
-      <LogEffect/>
-      <CalculadoraTrapecio/>
-      <DocumentTitleChanger/>
-      <ParentLoginForm/>
-      <PostLikes/>
-      <CheckboxSummary/>
-      <AccessWithLimit/>
-      <Greeting name="Alejandro" lastname="Martínez" />
-      <Greeting name="Ana" lastname="García" />
-      <UserCard user={userData} />
-      <ButtonOne onClick={() => alert("¡Hola desde el padre!")} />
-      <ParentIncrementButton />
-      <ParentInputNotifier />
+      <FocoAutomatico />
+      <CirculoColorido />
+      <ScrollDemo />
+      <ReproductorSonido />
+      <MoverCaja />
+      <ExpensiveCalc />
+      <FiltroLista />
+      <Contador />
     </>
   );
 }
+
 export default App;
